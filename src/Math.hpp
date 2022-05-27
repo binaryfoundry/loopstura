7d5f623015c4878106c8ff1e1bd3d49cbec938a4
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2022-05-27 11:41:49 +0100

    Interpolation Methods

--- src/Math.hpp
+++ src/Math.hpp
@@ -24,3 +24,47 @@ using glm::mat2;
 using glm::mat3;
 using glm::mat4;
 using glm::quat;
+
+// http://paulbourke.net/miscellaneous/interpolation/
+
+double CosineInterpolate(
+    double y1, double y2,
+    double mu)
+{
+    double mu2;
+
+    mu2 = (1 - cos(mu * std::numbers::pi)) / 2;
+    return(y1 * (1 - mu2) + y2 * mu2);
+}
+
+double CubicInterpolate(
+    double y0, double y1,
+    double y2, double y3,
+    double mu)
+{
+    double a0, a1, a2, a3, mu2;
+
+    mu2 = mu * mu;
+    a0 = y3 - y2 - y0 + y1;
+    a1 = y0 - y1 - a0;
+    a2 = y2 - y0;
+    a3 = y1;
+
+    return(a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3);
+}
+
+double CatmullRomInterpolate(
+    double y0, double y1,
+    double y2, double y3,
+    double mu)
+{
+    double a0, a1, a2, a3, mu2;
+
+    mu2 = mu * mu;
+    a0 = -0.5 * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3;
+    a1 = y0 - 2.5 * y1 + 2 * y2 - 0.5 * y3;
+    a2 = -0.5 * y0 + 0.5 * y2;
+    a3 = y1;
+
+    return(a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3);
+}
